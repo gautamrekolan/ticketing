eda09f534625d1e7bd8a5168d66f9fad918b7ad9
Davey <davey@ubuntu.ubuntu-domain>
2011-09-18 17:29:46 +0100

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,17 @@ simplicity = Theme.create(:name => "Simplicity")
 day_invitation = ProductType.create(:description => "Day Invitation")
 evening_invitation = ProductType.create(:description => "Evening Invitation")
 invitation_envelope = ProductType.create!(:description => "Invitation Envelope", :envelope => true)
+name_place = ProductType.create!(:description => "Name Place Card")
 c6_envelope = ProductFormat.create(:description => "C6")
 
 a6_flat = ProductFormat.create(:width => 105, :height => 148, :description => "A6 flat", :style => "FLAT")
+tent = ProductFormat.create(:width=> 74, :height => 105, :description => "Name place card", :style => "TENT")
 paper = Paper.new(:texture => "Smooth", :colour => "White", :weight => 80)
 product = Product.new(:theme => simplicity, :product_format => a6_flat, :product_type => day_invitation, :paper => paper, :price => 10)
 product2 = Product.new(:theme => simplicity, :product_format => a6_flat, :product_type => evening_invitation, :paper => paper, :price => 15)
 product3 = Product.new(:theme => simplicity, :product_format => c6_envelope, :product_type => invitation_envelope, :paper => paper, :price => 20)
-simplicity.products << product << product2 << product3
+name_place = Product.new(:theme => simplicity, :product_format => tent, :product_type => name_place, :paper => paper, :price => 15)
+simplicity.products << product << product2 << product3 << name_place
 
 rose = Theme.create!(:name => "Rose")
 product = Product.new(:theme => rose, :product_format => a6_flat, :product_type => day_invitation, :paper => paper, :price => 20)
@@ -52,6 +55,8 @@ message.reply_to_addresses << [ address7, address8 ]
 customer_address = CustomerAddress.create(:customer => customer, :name => "Shirley Pettifer", :address_1 => "22 Brook Road", :town => "South Benfleet", :county => "Essex", :country => "United Kingdom", :postcode => "SS7 5PJ")
 order = Order.create(:customer => customer, :customer_address => customer_address)
 order.items.build(:product => product, :quantity_ordered => 30, :price => 5)
+order.items.build(:product => product3, :quantity_ordered => 25, :price => 10)
+order.items.build(:product => name_place, :quantity_ordered => 15, :price => 15)
 order.save!
 
 
